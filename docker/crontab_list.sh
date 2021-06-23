8d37a3d33f0557e3ccb2f2d818d0f981914726b3
iouAkira <e.akimoto.akira@gmail.com>
2021-06-23 11:46:15 +0800

    remove expire task

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -4,26 +4,16 @@
 30 * * * * sh +x /scripts/docker/auto_help.sh collect >> /data/logs/auto_help_collect.log 2>&1
 
 ##############短期活动##############
-#女装盲盒 活动时间：2021-05-24到2021-06-22
-35 1,22 * * * node /scripts/jd_nzmh.js >> /data/logs/jd_nzmh.log 2>&1
-
 #京东极速版红包(活动时间：2021-5-5至2021-5-31)
 45 0,23 * * * node /scripts/jd_speed_redpocke.js >> /data/logs/jd_speed_redpocke.log 2>&1
-
 #超级直播间红包雨(活动时间不定期，出现异常提示请忽略。红包雨期间会正常)
 1,31 0-23/1 * * * node /scripts/jd_live_redrain.js >> /data/logs/jd_live_redrain.log 2>&1
-
 #每日抽奖(活动时间：2021-05-01至2021-05-31)
 13 1,22,23 * * * node /scripts/jd_daily_lottery.js >> /data/logs/jd_daily_lottery.log 2>&1
-
 #金榜创造营 活动时间：2021-05-21至2021-12-31
 0 1,22 * * * node /scripts/jd_gold_creator.js >> /data/logs/jd_gold_creator.log 2>&1
 #5G超级盲盒(活动时间：2021-06-2到2021-07-31)
 0 0-23/4 * * * node /scripts/jd_mohe.js >> /data/logs/jd_mohe.log 2>&1
-#明星小店(星店长，2021-06-10)
-0 1,21 * * * node /scripts/jd_star_shop.js >> /data/logs/jd_star_shop.log 2>&1
-#新潮品牌狂欢（6.18过期）
-20 1,21 * * * node /scripts/jd_mcxhd.js >> /data/logs/jd_mcxhd.log 2>&1
 #京喜领88元红包(6.31到期)
 30 1,6,12,21 * * * node /scripts/jd_jxlhb.js >> /data/logs/jd_jxlhb.log 2>&1
 #省钱大赢家之翻翻乐
